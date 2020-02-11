264bdd0548a614020541bc8dbdeaaa2fa2933e6a
Eric Feczko <feczko@ohsu.edu>
2020-02-11 12:21:11 -0800

    added new interface for nifti connectivity matrices -- niiconns

---
+++ R/PrepNiiConnMetric.R
@@ -0,0 +1,19 @@
+#' PrepNiiConnMetric -- A function for loading nifti connectivity metric data
+#' 
+#' This function is a simple wrapper for reading connectivity matrices from niftis. 
+#' The function will check to see whether the qform is needed before loading in the data.
+#' Generally, the qform shouldn't be needed, if it is used, there may be a warning when using the dataset.
+#' @param niiconn_file A character string representing the path to a nifti connectivity matrix file.
+#' @keywords nifti volume conn
+#' @export
+#' @examples 
+#' niftifile <- PrepNiiConnMetric(nifti_file)
+#' niftifiles <- lapply(nifti_filelist,PrepNiiConnMetric)
+PrepNiiConnMetric <- function(niiconn_file){
+  library("oro.nifti")
+  niiconnmat <- readNIfTI(niiconn_file,reorient = FALSE)
+  if (niiconnmat[1,1] != 1){
+    print("Warning: default load did not orient matrix properly -- using qform to flip matrix")
+    niiconnmat <- readNIfTI(niiconn_file,reorient = TRUE)
+  }
+}
